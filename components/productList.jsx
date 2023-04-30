37381149bb167ed616c3e3b6e22b53e97d450315
malik.ozturk <malik.ozturk@enuygun.com>
2023-04-30 16:44:11 +0300

    feat: add cart

--- components/productList.jsx
+++ components/productList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../styles/layout/ProductList.module.scss"
 import Link from "next/link"
 import Image from 'next/image'
@@ -6,14 +6,11 @@ import BreadCrumb from "./assets/svg/header/breadcrumb.svg"
 import ProductCounter from "./assets/svg/homepage/productCounter.svg"
 import Test from "./assets/jpg/test.jpg"
 import { tests } from '../data/test'
-import { useSelector, useDispatch } from "react-redux"
-import { actionProducts } from "../store/products"
 
 function ProductList() {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(actionProducts(tests))
-  }, [])
+  const addCart = () => {
+    console.log('sepete eklendi')
+  }
   return (
     <>
       <div className={styles.headerWrapper}>
@@ -47,31 +44,40 @@ function ProductList() {
             <div className={styles.main}>
               <div className={styles.wrapper}>
                 <div className={styles.cardWrapper}>
-                  <article className={styles.productMain}>
-                    <div className={styles.imageMain}>
-                      <Link href='/' className={styles.imageLink} scroll={false}>
-                        <figure className={styles.imageFigure}>
-                          <Image className={styles.image} src={Test} width={120} height={120} />
-                        </figure>
-                        <div className={styles.counterMain}>
-                          <div className={styles.wrapper}>
-                            <button className={styles.counterButton}>
-                              <div className={styles.iconMain}>
-                                <ProductCounter className={styles.icon} />
-                              </div>
-                            </button>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className={styles.priceMain}>
-                      <span className={styles.price}>₺35,95</span>
-                    </div>
-                    <span className={styles.titleMain}>Kuzeyden</span>
-                    <div className={styles.paragraphMain}>
-                      <p className={styles.paragraph}>6 x 1,5 L</p>
-                    </div>
-                  </article>
+                  {tests.map((test) => {
+                    test.subCategories.map((subCategory) => {
+                      subCategory.products?.map((product) => {
+                        console.log(product, 'product')
+                        return (
+                          <article className={styles.productMain}>
+                            <div className={styles.imageMain}>
+                              <Link href='/' className={styles.imageLink} scroll={false}>
+                                <figure className={styles.imageFigure}>
+                                  <Image className={styles.image} src={Test} width={120} height={120} />
+                                </figure>
+                                <div className={styles.counterMain} onClick={addCart}>
+                                  <div className={styles.wrapper}>
+                                    <button className={styles.counterButton}>
+                                      <div className={styles.iconMain}>
+                                        <ProductCounter className={styles.icon} />
+                                      </div>
+                                    </button>
+                                  </div>
+                                </div>
+                              </Link>
+                            </div>
+                            <div className={styles.priceMain}>
+                              <span className={styles.price}>{product.priceText}</span>
+                            </div>
+                            <span className={styles.titleMain}>Kuzeyden</span>
+                            <div className={styles.paragraphMain}>
+                              <p className={styles.paragraph}>6 x 1,5 L</p>
+                            </div>
+                          </article>
+                        )
+                      })
+                    })
+                  })}
                 </div>
               </div>
             </div>
